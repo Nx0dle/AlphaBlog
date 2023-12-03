61c0f21991135868c93001c31867340fcedab7af
Nx0dle <ramenkeebs@proton.me>
2023-12-03 19:02:36 +0100

    specify user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
