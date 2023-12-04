ff5d86bf3524096602e25fbcb9d97934ba2a3a8d
Nx0dle <ramenkeebs@proton.me>
2023-12-04 20:16:31 +0100

    Style articles - show, new, edit, index and messages - flash, errors

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@
 class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
-  validates :description, presence: true, length: {minimum: 10, maximum: 600}
+  validates :description, presence: true, length: {minimum: 10, maximum: 5000}
 end
